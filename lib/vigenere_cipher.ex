e9b6ce111cd5b7a2ed4be64dfd20aea88b7f379e
Glyph <171430588+AshenCinders@users.noreply.github.com>
2026-01-26 19:33:23 +0100

    feat: handle input args

--- lib/vigenere_cipher.ex
+++ lib/vigenere_cipher.ex
@@ -1,4 +1,36 @@
 defmodule VCipher.Main do
+  @spec main([binary()]) :: :ok | :error
   def main(args) do
+    {flags, _remaining, _invalid} = parse_flags(args)
+    # TODO handle invalid
+
+    case IO.read(:stdio, :eof) do
+      :eof ->
+        IO.puts("The text file was empty or errored!")
+        :error
+
+      {:error, reason} ->
+        IO.puts("Failed to read input text file: " <> reason)
+        :error
+
+      text ->
+        dispatch(text, flags)
+    end
+  end
+
+  defp parse_flags(args) do
+    OptionParser.parse(args,
+      strict: [encode: :boolean, decode: :boolean, key: :string],
+      aliases: [e: :encode, d: :decode, k: :key]
+    )
+  end
+
+  @spec dispatch(String.t(), OptionParser.parsed()) :: :ok | :error
+  defp dispatch(raw_text, flags) do
+    cond do
+      not List.keymember?(flags, :key, 0) ->
+        IO.puts("No key provided")
+        :error
+    end
   end
 end
